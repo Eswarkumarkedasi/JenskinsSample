fdb927f7f3f199ca1e47bb853a5cc3aedad7396a
Eswarkumar-k <eswar.kedasi@otsi.co.in>
2025-11-06 12:25:57 +0530

    Initial commit

---
+++ src/test/java/com/SampleJenkins/SampleJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SampleJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
